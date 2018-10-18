e26268eaf7a3c18cc39bbb056291bf704a238500
Den <den@mail.ru>
2018-10-18 14:53:15 +0300

    eq

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	Cat.find(function(err,cats){
+		res.render('index', { 
+			title: 'Express' ,
+			cats: cats
+		});
+	})
 });
 
 module.exports = router;
